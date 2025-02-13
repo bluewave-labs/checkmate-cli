3a4726dfb7bfefc2ae43a52994af9567fd5b33f6
Mert Şişmanoğlu <mert190737fb@gmail.com>
2025-02-13 20:10:17 +0300

    fix(output): Update monitor output template to handle empty states more gracefully
    Signed-off-by: Mert Şişmanoğlu <mert190737fb@gmail.com>

--- internal/cli/output/monitor.go
+++ internal/cli/output/monitor.go
@@ -3,12 +3,12 @@ package output
 var MonitorListTemplate = `Your Checkmate Monitors on instance {{green .InstanceURL}}
 Team ID: {{cyan .TeamID }}
 
-{{if gt (len .TotalMonitors) 0 -}}
-Total Monitors: {{blue (intToString (len .TotalMonitors)) }} 
+{{if and .TotalMonitors (gt (len .TotalMonitors) 0) -}}
+Total Monitors: {{blue (intToString (len .TotalMonitors))}}
 {{else}}
 There are no monitors
 {{- end }}
-{{if gt (len .UpMonitors) 0}}
-Up Monitors: {{green (intToString (len .UpMonitors))}} 
+{{if and .UpMonitors (gt (len .UpMonitors) 0)}}
+Up Monitors: {{green (intToString (len .UpMonitors))}}
 {{- else}}
 {{- end }}`
